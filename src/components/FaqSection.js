66758fe667c9048fbbb151763284cae646d45d3b
Sujon Mahamud <47375078+nasujon@users.noreply.github.com>
2021-08-08 11:28:59 +0600

    added faq styles

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import {About} from '../styles';
 const FaqSection = () => {
   return (
-    <div className="faqs">
+    <Faq>
 
       <h2>Any questions <span>Faq</span></h2>
       <div className="question">
@@ -12,6 +13,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eum?</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>How do i start ?</h4>
@@ -20,6 +22,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eum?</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>How do i start ?</h4>
@@ -28,9 +31,36 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eum?</p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   )
 }
+
+const Faq = styled(About)`
+  display: block;
+  span{
+    display:block;
+  }
+  h2{
+    padding: 10px;
+  }
+  h4{
+    font-size: 22px
+  }
+  .question{
+    padding: 15px 0;
+    cursor: pointer;
+  }
+  p{
+    padding:10px 0;
+  }
+  .answer{
+    padding: 10px 0;
+  }
+  .faq-line{
+    border-bottom: 1px solid #fff;
+  }
+`;
 
 export default FaqSection

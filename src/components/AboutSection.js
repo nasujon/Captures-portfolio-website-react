600041c0aea13bd0c6b17fe9750089eb2a24b326
Sujon Mahamud <47375078+nasujon@users.noreply.github.com>
2021-08-10 10:29:08 +0600

    added animation test

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,23 +3,70 @@ import { About, Description, Image } from '../styles';
 import styled from 'styled-components';
 //Images
 import homeImg1 from '../img/home1.png';
+
+//animtion framer motion
+import {motion} from 'framer-motion'
+
+
 const AboutSection = () => {
+
+  const titleAnim = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        duration:2
+      }
+    }
+  }
+
+  const container = {
+    hidden:{
+      x:100
+    },
+    show: {
+      x:0,
+      
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 1,
+        when: "afterChildren"
+      }
+    }
+  }
+  const peraAnim = {
+    hidden: {
+      x:10,
+      opacity: 0
+    },
+    show:{
+      x:0,
+      opacity: 1,
+      transition: {
+        duration:1.25,
+        
+      }
+    }
+  }
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div variants={container} animate="show" initial="hidden" className="title">
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim} animate="show" initial="hidden">We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim} animate="show" initial="hidden">
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={peraAnim} initial="hidden" animate="show">true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills to help you achieve it.

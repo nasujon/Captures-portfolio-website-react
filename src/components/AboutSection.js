e337c58dfd846c5b29a6204ddbadb98907c4dcc8
Sujon Mahamud <47375078+nasujon@users.noreply.github.com>
2021-08-10 12:09:47 +0600

    about page animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,75 +6,34 @@ import homeImg1 from '../img/home1.png';
 
 //animtion framer motion
 import {motion} from 'framer-motion'
-
+import {titleAnim, fade, photoAnim} from '../animations'
 
 const AboutSection = () => {
 
-  const titleAnim = {
-    hidden: {
-      opacity: 0,
-    },
-    show: {
-      opacity: 1,
-      transition: {
-        duration:2
-      }
-    }
-  }
-
-  const container = {
-    hidden:{
-      x:100
-    },
-    show: {
-      x:0,
-      
-      transition: {
-        duration: 1,
-        ease: "easeOut",
-        staggerChildren: 1,
-        when: "afterChildren"
-      }
-    }
-  }
-  const peraAnim = {
-    hidden: {
-      x:-10,
-      opacity: 0
-    },
-    show:{
-      x:0,
-      opacity: 1,
-      transition: {
-        duration:1.25,
-        
-      }
-    }
-  }
   return (
     <About>
       <Description>
-        <motion.div variants={container} animate="show" initial="hidden" className="title">
+        <motion.div className="title">
           <Hide>
-            <motion.h2 variants={titleAnim} animate="show" initial="hidden">We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim} animate="show" initial="hidden">
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={peraAnim} initial="hidden" animate="show">true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade} >
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills to help you achieve it.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade} >Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={homeImg1} alt="camera guy" />
+        <motion.img variants={photoAnim}  src={homeImg1} alt="camera guy" />
       </Image>
       
     </About>

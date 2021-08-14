487df7cbd69c559686aadface51f6128f1a918e3
Sujon Mahamud <47375078+nasujon@users.noreply.github.com>
2021-08-14 11:47:39 +0600

    added work page animaton

--- src/pages/OurWorks.js
+++ src/pages/OurWorks.js
@@ -8,7 +8,14 @@ import goodtimes from '../img/goodtimes-small.png';
 //Animation
 
 import {motion} from 'framer-motion';
-import { pageAnimation } from '../animations';
+import { 
+  pageAnimation, 
+  fade, 
+  photoAnim, 
+  lineAnim, 
+  slider, 
+  sliderContainer
+} from '../animations';
 
 
 
@@ -16,12 +23,22 @@ const OurWork = () => {
   return (
     <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit">
 
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
 
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-            <img src={athelete} alt="athlete" />
+        
+          <Hide>
+            <motion.img variants={photoAnim} src={athelete} alt="athlete" />  
+          </Hide>
+            
         </Link>
       </Movie>
       <Movie>
@@ -66,7 +83,36 @@ const Movie = styled.div`
     height: 70vh;
     object-fit: cover;
   }
+
+ 
+
 `;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
+
 
 
 export default OurWork;
